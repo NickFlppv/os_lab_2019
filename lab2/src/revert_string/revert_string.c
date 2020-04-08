7111a932e0c7894d77f77d0ca73934b2826ba90a
nickflppv <nikolay.filippov.1999@mail.ru>
2020-04-08 14:16:17 +0000

    Completed task 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
+#include "swap.h"
 #include "revert_string.h"
+#include <string.h>
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str) {
+  int length = strlen(str);
+  char* begin = str;
+  char* end = str;
+
+  for (int c = 0; c < length - 1; c++)
+    end++;
+
+  for (int c = 0; c < length / 2; c++) {
+    Swap(begin, end);
+    begin++;
+    end--;
+  }
 }
-
