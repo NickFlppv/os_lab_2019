e498c015e340c059e464027ac2fb46ecc2c15f54
nickflppv <nikolay.filippov.1999@mail.ru>
2020-04-24 15:07:36 +0000

    additional task

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,18 +1,19 @@
-#include "swap.h"
 #include "revert_string.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void RevertString(char *str) {
-  int length = strlen(str);
-  char* begin = str;
-  char* end = str;
-
-  for (int c = 0; c < length - 1; c++)
-    end++;
-
-  for (int c = 0; c < length / 2; c++) {
-    Swap(begin, end);
-    begin++;
-    end--;
-  }
+    int shift = 3;
+    int len = strlen(str)/2 + shift;
+    //char *new_str = malloc(sizeof(char) * len);
+    int i;
+    char buff;
+    for(i = strlen(str)/2 - shift; i < len + 1;i++, len--)
+    {
+        buff = str[i];
+        str[i] = str[len];
+        str[len] = buff;
+        //printf("%c %d %d\n",new_str[i], i, len);
+    }
 }
