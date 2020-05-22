dbf595c3cc53c860f15fbfa9b203c6ee03f6249b
nickflppv <nikolay.filippov.1999@mail.ru>
2020-05-22 16:15:38 +0000

    deadlock

--- lab5/src/deadlock/deadlock.c
+++ lab5/src/deadlock/deadlock.c
@@ -4,10 +4,10 @@
 
 static pthread_mutex_t mtx_first = PTHREAD_MUTEX_INITIALIZER;
 static pthread_mutex_t mtx_second = PTHREAD_MUTEX_INITIALIZER;
+int i=0;
 
 
 static void function1() {
-    int i=0;
     printf("first function\n");
     pthread_mutex_lock(&mtx_first);
     do{
@@ -16,11 +16,10 @@ static void function1() {
     }while(1);
     // pthread_mutex_lock(&mtx_second);
     // pthread_mutex_unlock(&mtx_second);
-    pthread_mutex_unlock(&mtx_first);
+    // pthread_mutex_unlock(&mtx_first);
 }
 
 static void function2() {
-    int i=0;
     printf("second function\n");
     // pthread_mutex_lock(&mtx_second);
     pthread_mutex_lock(&mtx_first);
@@ -44,15 +43,15 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    // if (pthread_join(t1, 0) != 0) {
-    //     printf("Error: cannot join first thread\n");
-    //     return -1;
-    // }
+    if (pthread_join(t1, 0) != 0) {
+        printf("Error: cannot join first thread\n");
+        return -1;
+    }
 
-    // if (pthread_join(t2, 0) != 0) {
-    //     printf("Error: cannot join second thread\n");
-    //     return -1;
-    // }
+    if (pthread_join(t2, 0) != 0) {
+        printf("Error: cannot join second thread\n");
+        return -1;
+    }
 }
 
 //gcc deadlock.c -lpthread -o deadlock.o && ./deadlock.o
